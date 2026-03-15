e6042656f00ee5df4e02a18fa42f4f3c65be768b
khannasujaan <khannasujaan@gmail.com>
2026-03-15 14:48:25 +0530

    fix: change the use of SECONDS variable to date command

---
+++ MonkeyType/monkeytype.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+RANDOM=$$
+
+min(){
+    if [ $1 -le $2 ]; then
+        echo $1
+    else 
+        echo $2
+    fi
+}
+
+while true; do
+
+    x=$(($RANDOM%20+1))
+    sentence="$(head -n $x sentences.txt | tail -n 1)"
+    echo $sentence
+    echo "Press Enter to start"
+
+    read temp
+    startTime=$(date +%s%N)
+    read typedInput
+    endTime=$(date +%s%N)
+    totalTime=$(echo "($endTime - $startTime)/1000000000" | bc)
+    len=${#sentence}
+    typedLength=${#typedInput}
+    n=$(min $len $typedLength)
+
+    correctChar=0
+    wrongChar=0
+    for (( i=0; i<$n; i++ ));
+    do 
+        if [ "${sentence:$i:1}" == "${typedInput:$i:1}" ]; then
+            ((correctChar++))
+        else 
+            ((wrongChar++))
+        fi
+    done
+    accuracy=$(echo "scale=4; $correctChar*100 / $len" | bc)
+
+    echo "Accuracy: $accuracy"
+    spaceSegment=0
+    for (( j=0; j<$typedLength; j++ ));
+    do 
+        if [ "${typedInput:j:1}" == " " ]; then
+            ((spaceSegment++))
+            while [ "${typedInput:j:1}" == " " ];
+            do 
+                ((j++))
+            done
+        fi
+    done
+    WPM=$(echo "scale=4; ($spaceSegment+1)*60/$totalTime" | bc)
+    echo "WPM: $WPM"
+    echo
+    echo "Do you want to repeat the speedtest (y/n):- "
+    read d
+    if [ $d != "y" ]; then
+        break
+    fi
+done
