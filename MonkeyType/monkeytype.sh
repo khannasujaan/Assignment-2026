c75b34755719a2488e2e8402715cd26f3f8d9061
khannasujaan <khannasujaan@gmail.com>
2026-03-15 17:45:44 +0530

    feat: change input to char-by-char

--- MonkeyType/monkeytype.sh
+++ MonkeyType/monkeytype.sh
@@ -14,13 +14,46 @@ while true; do
     x=$(($RANDOM%20+1))
     sentence="$(head -n $x sentences.txt | tail -n 1)"
     echo $sentence
+    len=${#sentence}
 
     read -p "Press Enter to start"
     startTime=$(date +%s%N)
-    read -t 60 typedInput
+    echo "Time STARTED"
+    typedInput=""
+    currentPos=0
+    keysPressed=0
+    green=0
+    red=0
+    pointer=0
+    while true; do
+        if [[ $(($(date +%s%N) - $startTime)) -ge 60000000000 ]]; then
+            break
+        fi
+        now=$(date +%s%N)
+        elapsed=$(( (now - startTime) / 1000000000 ))
+        timeLeft=$(( 60 - elapsed ))
+        IFS= read -n 1 -s -r -t "$timeLeft" charInput
+        if [[ $charInput == $'' ]]; then 
+            break
+        elif [[ $charInput == $'\x7f' && $currentPos -ge 2 ]]; then
+            ((currentPos-=2))
+            typedInput="${typedInput%?}"
+            echo -e -n "\b \b"
+        else
+            typedInput+=$charInput
+            if [[ $charInput == $' ' ]]; then
+                echo -n " "
+            else
+                echo -n $charInput
+            fi
+        fi
+        ((currentPos+=1))
+        ((keysPressed+=1))
+    done
+    echo
+        
     endTime=$(date +%s%N)
     totalTime=$(echo "($endTime - $startTime)/1000000000" | bc)
-    len=${#sentence}
     typedLength=${#typedInput}
     n=$(min $len $typedLength)
 
